6d335270eefcc512b9fecc5204c169abc2a5ab17
Bohdan Buchok <bohdan.buchok@internetbrands.com>
2026-02-05 17:36:55 +0200

    feat:

--- server_k8s/post-setup.sh
+++ server_k8s/post-setup.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 /usr/bin/apt-get update
-/usr/bin/apt-get install -y sudo vim net-tools openssh-server ifupdown curl wget
+/usr/bin/apt-get install -y sudo vim net-tools openssh-server ifupdown curl wget rsync
 
 update-alternatives --set editor /usr/bin/vim.basic
-systemctl disable firewalld --now
+#sudo systemctl disable nftables --now
+
+# TODO tailscale
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-k12Nr8Dw3z11CNTRL-TCcYpmmXBqNdCqS3emzzqN14pgk7QMB8
 
 # The industry standard is to avoid editing the main /etc/sudoers file directly. Instead, you should drop a configuration file into the /etc/sudoers.d/ directory. It’s cleaner, modular, and much harder to break.
-echo "%sudo ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers.d/nopasswd_sudo
+# echo "%sudo ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers.d/nopasswd_sudo
+# gpasswd -a bchk sudo
 
-gpasswd -a bchk sudo
+curl -sfL https://get.k3s.io | sh -
 
 mkdir -p ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
@@ -17,6 +21,14 @@ sudo chown $USER:$USER ~/.kube/config
 chmod 600 ~/.kube/config
 export KUBECONFIG=$HOME/.kube/config
 kubectl get nodes
+
+
+
+# sudo systemctl edit k3s.service
+# [Service]
+# ExecStart=
+# ExecStart=/usr/local/bin/k3s server --data-dir /home/k3s-data
+
 
 
 # ---------
